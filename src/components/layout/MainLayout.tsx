65e624e857fd9b07e6fa89176d4832460dae9574
absi0416 <>
2024-03-13 19:16:07 +0530

    Main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+/*import { Outlet } from "react-router-dom";
 import { Box, Toolbar } from "@mui/material";
 import colorConfigs from "../../configs/colorConfigs";
 import sizeConfigs from "../../configs/sizeConfigs";
@@ -58,6 +58,50 @@ const MainLayout = () => {
       <Outlet />
     </Box>
   </Box>)};
+};
+
+export default MainLayout;*/
+
+import { Outlet, useLocation } from "react-router-dom";
+import { Box, Toolbar } from "@mui/material";
+import colorConfigs from "../../configs/colorConfigs";
+import sizeConfigs from "../../configs/sizeConfigs";
+import Sidebar from "../common/Sidebar";
+import Topbar from "../common/Topbar";
+
+const MainLayout = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === "/login"; // Assuming "/login" is your login route
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Topbar />
+      {!isLoginPage && ( // Render Sidebar only if not on the login page
+        <Box
+          component="nav"
+          sx={{
+            width: sizeConfigs.sidebar.width,
+            flexShrink: 0
+          }}
+        >
+          <Sidebar />
+        </Box>
+      )}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: isLoginPage ? "100%" : `calc(100% - ${sizeConfigs.sidebar.width})`, // Adjust width based on login page
+          minHeight: "100vh",
+          backgroundColor: colorConfigs.mainBg
+        }}
+      >
+        <Toolbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default MainLayout;
